15bfaae88052dc42f5bfae3b2b9db569a1a1de0e
KumarManish12 <mkb.42700@gmail.com>
2023-05-15 15:05:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BroadcastActivity
+ Broadcast Receiver
---
+++ app/src/main/java/com/example/broadcastactivity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.broadcastactivity
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.broadcastactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var myReceiver: MyReceiver
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        myReceiver=MyReceiver()
+        var intentFilter = IntentFilter("Hello")
+        registerReceiver(myReceiver,intentFilter)
+        binding.btntestBroadcast.setOnClickListener {
+            val intent=Intent("Hello")
+            intent.putExtra("count",count++)
+            sendBroadcast(intent)
+        }
+    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        unregisterReceiver(myReceiver)
+//    }
+}
